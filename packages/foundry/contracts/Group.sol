4645cb3168db5e3340a89946699a2365d7e0a5a1
sh4dex <sthx09567541@gmail.com>
2026-05-17 13:48:02 -0500

    build: finish contracts for V1 with payAll function

--- packages/foundry/contracts/Group.sol
+++ packages/foundry/contracts/Group.sol
@@ -3,9 +3,13 @@
 pragma solidity 0.8.33;
 
 contract Group {
-
-    event CreatedGroup( address creator,address  groupAddress, address[] initialMembers); 
+    event CreatedGroup(
+        address creator,
+        address groupAddress,
+        address[] initialMembers
+    );
     event ExpenseAdded(address payer, uint256 amount);
+    event DebtsPaid(address payer, uint256 totalOwned);
 
     uint256 totalMembers;
     address public registry;
@@ -20,14 +24,22 @@ contract Group {
         uint256 value;
     }
     Debt[] public debts;
-    
+
+    modifier onlyMember() {
+        require(isMember[msg.sender], "Not a member of the group");
+        _;
+    }
 
     /**
-     * @notice initialize the mapping of balances all in zeros for 
+     * @notice initialize the mapping of balances all in zeros for
      * every single member of the group.
      */
-    constructor(address registry_, address creator_, address[] memory members_) {
-        require(members_.length > 0,"Can't create a group with no members");
+    constructor(
+        address registry_,
+        address creator_,
+        address[] memory members_
+    ) {
+        require(members_.length > 0, "Can't create a group with no members");
         creator = creator_;
         registry = registry_;
         for (uint i = 0; i < members_.length; i++) {
@@ -36,20 +48,19 @@ contract Group {
             balances[members_[i]] = 0;
         }
         totalMembers = members_.length;
-        emit CreatedGroup(msg.sender ,address(this), members);
-
+        emit CreatedGroup(msg.sender, address(this), members);
     }
 
     /**
-     * 
+     *
      * @param payer_ Address that have pay for the expense
      * @param amount_ amount paid
      */
-    function addExpense(address payer_, uint256 amount_)  public {
+    function addExpense(address payer_, uint256 amount_) public {
         require(isMember[payer_], "Payer is not in the group");
-        require(amount_ > 0, "Amount can't be 0");
+        require(amount_ > 0, "Amount can't be 0 or less");
 
-        uint256 splitValue = (amount_ * 1e18 ) / totalMembers;
+        uint256 splitValue = (amount_ * 1e18) / totalMembers;
         for (uint i = 0; i < members.length; i++) {
             balances[members[i]] -= int256(splitValue / 1e18);
         }
@@ -58,74 +69,92 @@ contract Group {
         emit ExpenseAdded(payer_, amount_);
     }
 
-    /** 
+    /**
      * @notice split every single time that balances are recalculated
      * This function is to get the value that every member needs to pay
      * to each other. (make liquidation)
      */
-    function split()  public {
+    function split() public {
+        for (uint i = 0; i < debts.length; i++) {
+            require(
+                debts[i].value == 0,
+                "Outstanding debts must be settled first"
+            );
+        }
         uint256 n_deptors; //negative balance
         uint256 n_creditors; //positive balance
 
         uint256 n = members.length;
         for (uint i = 0; i < n; i++) {
-            if (balances[members[i]] >= 0) n_creditors +=1;
+            if (balances[members[i]] >= 0) n_creditors += 1;
             else n_deptors += 1;
         }
-
         //Complete the two aux lists
         address[] memory deptors = new address[](n_deptors);
         address[] memory creditors = new address[](n_creditors);
-        uint256 i_deptors; 
+        uint256 i_deptors;
         uint256 i_creditors;
         for (uint i = 0; i < n; i++) {
-            if (balances[members[i]] >= 0){
-                creditors[i_creditors]=members[n];
-                i_creditors +=1;
-            }else{
-                deptors[i_deptors]=members[n];
-                i_deptors+=1;
-            } 
+            if (balances[members[i]] >= 0) {
+                creditors[i_creditors] = members[i];
+                i_creditors += 1;
+            } else {
+                deptors[i_deptors] = members[i];
+                i_deptors += 1;
+            }
         }
-        
-        //TODO: Check sorting
-        //Sort Them.
-        //TODO: Make both implementations, Checked If the lists are sorted:
-        // 1. Sort the list on Chain as exercise. (on-chain)
-        // 2. Procced with the list with sorting from Frontend (off-chain)
         getDebts(deptors, creditors);
-        // The value of all debts should be equal to the totalexpenses
+        //TODO: Review On-Chain Sorting
     }
 
     /**
-     * @notice Generate the individuals dev of each member of the group and each is 
+     * Returns true if the array is sorted or reverts with 'Array is not sorted'
+     * @param array_ Array to sort
+     */
+    function isSorted(
+        address[] memory array_
+    ) internal view returns (bool sorted) {
+        for (uint i = 0; i < array_.length - 1; i++) {
+            require(
+                balances[array_[i]] <= balances[array_[i + 1]],
+                "Array is not sorted"
+            );
+        }
+        return true;
+    }
+
+    /**
+     * @notice Generate the individuals dev of each member of the group and each is
      * register directly into ´debts´ array
      * @dev Requiere deptors and creditors arrays already sorted in decremental order
-     * example [addr(1), addr(2), addr(3)] -> addr(1) on index Zero should have the 
-     * Biggest value and addr(3) on index 2 should have the address with the smallest 
+     * example [addr(1), addr(2), addr(3)] -> addr(1) on index Zero should have the
+     * Biggest value and addr(3) on index 2 should have the address with the smallest
      * balance.
      * @param creditors_ Address with positive Balance sorted decrementaly
      * @param deptors_ Addresses with negative Balance sorted decrementaly
      */
-    function getDebts(address[] memory deptors_, address[] memory creditors_) public {
+    function getDebts(
+        address[] memory deptors_,
+        address[] memory creditors_
+    ) internal {
         uint256 j; //Deptor pointer
         uint256 leftCredit;
         uint256 leftDebt = abs(balances[deptors_[j]]);
         for (uint i = 0; i < creditors_.length; i++) {
             leftCredit = abs(balances[creditors_[i]]);
 
-            while(leftDebt > 0 && j < deptors_.length){ 
-                //Debt is bigger than the credit 
-                if(leftCredit >= leftDebt){
-                    addDebt(deptors_[j],creditors_[i],leftDebt);
+            while (leftDebt > 0 && j < deptors_.length) {
+                //Debt is bigger than the credit
+                if (leftCredit >= leftDebt) {
+                    addDebt(deptors_[j], creditors_[i], leftDebt);
                     leftCredit = leftCredit - leftDebt; //(pays all the Debt)
-                    leftDebt = 0; //next debtor 
+                    leftDebt = 0; //next debtor
                     j++;
-                    if(j<deptors_.length){
+                    if (j < deptors_.length) {
                         leftDebt = abs(balances[deptors_[j]]);
                     }
-                } else{
-                    addDebt(deptors_[j],creditors_[i],leftCredit);
+                } else {
+                    addDebt(deptors_[j], creditors_[i], leftCredit);
                     leftDebt -= leftCredit;
                     leftCredit = 0; //(breaks while in next iteration)
                 }
@@ -139,18 +168,14 @@ contract Group {
      * @param to_ address of creditor
      * @param value_ value debt
      */
-    function addDebt(address from_,address to_, uint256 value_)  internal {
-        debts.push(Debt({
-            from: from_,
-            to: to_,
-            value: value_
-        }));
+    function addDebt(address from_, address to_, uint256 value_) internal {
+        debts.push(Debt({from: from_, to: to_, value: value_}));
     }
 
     //return absolute value of a number
-    function abs(int256 x_)  internal pure returns (uint256)  {
-        require (x_ != type(int256).min, "Overflow");
-        return x_ >= 0 ? uint256(x_): uint256(-x_);
+    function abs(int256 x_) internal pure returns (uint256) {
+        require(x_ != type(int256).min, "Overflow");
+        return x_ >= 0 ? uint256(x_) : uint256(-x_);
     }
 
     /**
@@ -158,18 +183,40 @@ contract Group {
      * @dev By default it's balance is zero and is also
      * added to the registry map
      */
-    function addMember()  public {
-        //A member can't be in the group twice
+    function addMember() public {
+        //TODO:A member can't be in the group twice
     }
 
-    
     /**
-     * @notice transfer values stablished in the debt mapping and erase it after it.
+     * //TODO: pay a specific debt
      */
-    function pay() public {
-        // TODO: make payable -> pay a certain Debt req(the debtor should be message sender.)
-        // the value should not be greatest that the debt value.
+    function pay() public payable onlyMember {}
 
+    /**
+     * @notice transfer all values stablished in the debt mapping and erase it after it.
+     */
+    function payAll() public payable onlyMember {
+        uint256 totalOwed;
+        for (uint i = 0; i < debts.length; i++) {
+            if (debts[i].from == msg.sender) {
+                totalOwed += debts[i].value;
+            }
+        }
+        require(totalOwed > 0, "no debts to pay");
+        require(totalOwed == msg.value, "Must send exact debts total");
+
+        for (uint i = 0; i < debts.length; i++) {
+            if (debts[i].from == msg.sender && debts[i].value > 0) {
+                uint256 amount = debts[i].value;
+                balances[msg.sender] += int256(amount);
+                balances[debts[i].to] -= int256(amount);
+                debts[i].value = 0;
+
+                (bool success, ) = debts[i].to.call{value: amount}("");
+                require(success, "Transfer failed");
+            }
+        }
+        emit DebtsPaid(msg.sender, totalOwed);
     }
 
     /**
@@ -178,5 +225,4 @@ contract Group {
     function getMembers() public view returns (address[] memory members_) {
         members_ = members;
     }
-
 }
--- packages/foundry/test/Splitter.t.sol
+++ packages/foundry/test/Splitter.t.sol
@@ -7,14 +7,14 @@ import {Group} from "../contracts/Group.sol";
 import {RegistryGroups} from "../contracts/RegistryGroups.sol";
 
 contract Splitter is Test {
-
-//TODO: Implement Fuzztesting for different expenses and payments.
+    //TODO: Implement Fuzztesting for different expenses and payments.
 
     RegistryGroups rg;
     Group testGroup;
+
     function setUp() public {
         rg = new RegistryGroups(vm.addr(1));
-        address[]  memory members_ = new address[](4);
+        address[] memory members_ = new address[](4);
         members_[0] = makeAddr("thomas");
         members_[1] = makeAddr("camilo");
         members_[2] = makeAddr("Jeisson");
@@ -25,13 +25,13 @@ contract Splitter is Test {
 
     // REGISTRY TESTS
 
-    function testRegistryGroupDeployedPropperly() external view{
+    function testRegistryGroupDeployedPropperly() external view {
         assert(address(rg) != address(0));
         assert(rg.owner() == vm.addr(1));
     }
 
     function testCreateGroupProperly() external {
-        address[]  memory members_ = new address[](3);
+        address[] memory members_ = new address[](3);
         members_[0] = makeAddr("thomas");
         members_[1] = makeAddr("camilo");
         members_[2] = makeAddr("Jeisson");
@@ -50,17 +50,32 @@ contract Splitter is Test {
         address debtor = testGroup.members(1);
         uint256 amount_ = 150000;
         testGroup.addExpense(payer_, 150000);
-        uint256 splitValue = (amount_ * 1e18 ) / testGroup.getMembers().length;
-        splitValue = splitValue/1e18;
-        assert(testGroup.abs(testGroup.balances(payer_)) == amount_- splitValue);
+        uint256 splitValue = (amount_ * 1e18) / testGroup.getMembers().length;
+        splitValue = splitValue / 1e18;
+        assert(
+            testGroup.abs(testGroup.balances(payer_)) == amount_ - splitValue
+        );
         assert(testGroup.abs(testGroup.balances(debtor)) == splitValue);
-
     }
 
+    function testAddExpenseDoNotAcceptNegatives() external {
+        vm.expectRevert("Amount can't be 0 or less");
+        address payer_ = testGroup.members(0);
+        address debtor = testGroup.members(1);
+        uint256 amount_ = -1150000;
+        testGroup.addExpense(payer_, amount_);
+    }
+
+    //TODO: Test debts array after addExpense, check if the debts are created properly.
+    //TODO: Chekc pay all Fucntion to pay every single debt, check balances and debts after performing it.
+    //TODO: Check Split function (revert with unsorted), performed if sorted
+    //TODO: Add continuos expenses
+    //TODO: Add continuos expenses and payments and check balances and debts after each one.
+
     // function testMessageOnDeployment() public view {
-        // require(
-            // keccak256(bytes(yourContract.greeting())) ==
-                // keccak256("Building Unstoppable Apps!!!")
-        // );
+    // require(
+    // keccak256(bytes(yourContract.greeting())) ==
+    // keccak256("Building Unstoppable Apps!!!")
+    // );
     // }
 }
